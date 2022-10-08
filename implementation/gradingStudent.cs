be13e6920d614f93fe85de03e242e43c709dc982
badizz <badizz@users.noreply.github.com>
2022-10-08 16:47:51 +0300

    grading student

---
+++ implementation/gradingStudent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class gradingStudent
+{
+    // solution 1
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        for (int i = 0; i < grades.Count; i++){
+            int tmpGrade = grades[i];
+            if (tmpGrade % 5 != 0 && tmpGrade > 37){
+                do
+                {
+                    tmpGrade++;
+                } while (tmpGrade % 5 != 0);
+
+                if ((tmpGrade - grades[i]) < 3){
+                    grades[i] = tmpGrade;
+                }
+            }
+        }
+        return grades;
+    }
+
+    // other way
+    public static List<int> gradingStudents2(List<int> grades)
+    {
+        for (int i = 0; i < grades.Count; i++){
+            int tmpGrade = grades[i];
+            if (tmpGrade > 37){
+                int diff = 5 - (tmpGrade % 5);
+                if (diff < 3) {
+                    grades[i] = tmpGrade + diff;
+                }
+            }
+        }
+        return grades;
+    }
+    
+}
