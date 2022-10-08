93127f5dd128b896f345c05ee229be3ac76d5dc0
badizz <badizz@users.noreply.github.com>
2022-10-08 16:52:56 +0300

    added solution includes ternary operator for gradingStudent

--- implementation/gradingStudent.cs
+++ implementation/gradingStudent.cs
@@ -35,5 +35,15 @@ public class gradingStudent
         }
         return grades;
     }
-    
+
+    // Solution includes Ternary Operator
+    public static List<int> gradingStudents3(List<int> grades)
+    {
+        for (int i = 0; i < grades.Count; i++)
+        {
+            grades[i] = (grades[i] < 38 || (grades[i] % 5 < 3)) ? grades[i] : (grades[i] + (5 - grades[i] % 5));
+        }
+        return grades;
+    }
+
 }
