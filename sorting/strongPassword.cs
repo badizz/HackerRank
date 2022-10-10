f76cba299b10a0c3493e85ba9a13b21df5f55be4
badizz <badizz@users.noreply.github.com>
2022-10-11 00:45:44 +0300

    strongPassword was updated.

--- sorting/strongPassword.cs
+++ sorting/strongPassword.cs
@@ -22,4 +22,37 @@ public class strongPassword
 
         return Math.Max(requiredMinNum, 6 - n);
     }
+
+    //other solution
+    public static int minimumNumber2(int n, string password)
+    {
+        string numbers = "0123456789";
+        string lower_case = "abcdefghijklmnopqrstuvwxyz";
+        string upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string special_characters = "!@#$%^&*()-+";
+        bool numFlag = false, lowerFlag = false, upperFlag = false, specialFlag = false;
+        int minNumberCount = 0;
+
+        foreach (char c in password){
+            if (numbers.Contains(c))
+                numFlag = true;
+            if (lower_case.Contains(c))
+                lowerFlag = true;
+            if (upper_case.Contains(c))
+                upperFlag = true;
+            if (special_characters.Contains(c))
+                specialFlag = true;
+        }
+
+        if (!numFlag)
+            minNumberCount++;
+        if (!lowerFlag)
+            minNumberCount++;
+        if (!upperFlag)
+            minNumberCount++;
+        if (!specialFlag)
+            minNumberCount++;
+
+        return (n > 6 || (6 - n < minNumberCount)) ? minNumberCount : 6 - n;
+    }
 }
