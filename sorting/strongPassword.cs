a7ff308c0df6c4e27ff2774b53c2952f5fdec864
badizz <badizz@users.noreply.github.com>
2022-10-11 00:24:03 +0300

    strongPassword was added

---
+++ sorting/strongPassword.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class strongPassword
+{
+    public static int minimumNumber(int n, string password)
+    {
+        // Return the minimum number of characters to make the password strong
+        string special_characters = "!@#$%^&*()-+";
+        int requiredMinNum = 0;
+
+        if (!password.Any(char.IsDigit))
+            requiredMinNum++;
+
+        if (!password.Any(char.IsLower))
+            requiredMinNum++;
+
+        if (!password.Any(char.IsUpper))
+            requiredMinNum++;
+
+        if (!password.Intersect(special_characters).Any())
+            requiredMinNum++;
+
+        return Math.Max(requiredMinNum, 6 - n);
+    }
+}
