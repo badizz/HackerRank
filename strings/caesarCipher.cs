e4dde3aff0285f73e457466d425ca62127206b99
badizz <badizz@users.noreply.github.com>
2022-10-17 23:22:36 +0300

    caesarCipher(strings) was added

---
+++ strings/caesarCipher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class caesarCipher
+{
+    public static string caesarCipher(string s, int k)
+    {
+        char[] upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        char[] lowerAlphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+        StringBuilder result = new StringBuilder();
+
+        foreach (char c in s)
+        {
+            int letterIndex = 0;
+            int tmp = 0;
+            if (!char.IsLetter(c)){
+                result.Append(c);
+            }
+            else {
+                if (Char.IsUpper(c)){
+                    letterIndex = Array.IndexOf(upperAlphabet, c);
+                    tmp = (letterIndex + k) % 26;
+                    result.Append(upperAlphabet[tmp]);
+                }
+                else{
+                    letterIndex = Array.IndexOf(lowerAlphabet, c);
+                    tmp = (letterIndex + k) % 26;
+                    result.Append(lowerAlphabet[tmp]);
+                }
+            }
+        }
+        return result.ToString();
+    }
+}
