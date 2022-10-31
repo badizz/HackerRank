64622af1c7dbd90ebd9a8234e980c8ac6b3ec678
badizz <badizz@users.noreply.github.com>
2022-10-31 23:27:25 +0300

    twoStrings - other way

--- strings/twoStrings.cs
+++ strings/twoStrings.cs
@@ -11,4 +11,26 @@ public class twoStrings
         else
             return "NO";
     }
+
+    // solution 2
+    public static string twoStrings2(string s1, string s2)
+    {
+        char[] s1Arr = s1.ToCharArray();
+        return (s2.IndexOfAny(s1Arr) != -1) ? "YES" : "NO";
+    }
+
+    // the other way
+    public static string twoStrings(string s1, string s2)
+    {
+        HashSet<char> characterSet = new HashSet<char>();
+        foreach (char c in s1){
+            characterSet.Add(c);
+        }
+        foreach (char cx in s2){
+            if (characterSet.Contains(cx))
+                return "YES";
+        }
+        return "NO";
+    }
+
 }
