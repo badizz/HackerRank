5cd6bed2813e213df5cce88ed2d1b8b9c70d3c58
badizz <badizz@users.noreply.github.com>
2022-11-02 22:36:19 +0300

    marsExploration

---
+++ strings/marsExploration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class marsExploration
+{
+    public static int marsExploration(string s)
+    {
+        int result = 0;
+        int count = s.Length / 3;
+        int indexO = 1;
+        for (int i = 0; i < s.Length; i++){
+            if (i == indexO){
+                if (s[i] != 'O'){
+                    result++;
+                }
+                indexO += 3;
+            }
+            else {
+                if (s[i] != 'S')
+                    result++;
+            }
+        }
+        return result;
+    }
+}
