a33ce5bc2cbf6830e95c7a8a204bbdd1a4467186
badizz <badizz@users.noreply.github.com>
2022-11-02 23:01:10 +0300

    marsExploration update

--- strings/marsExploration.cs
+++ strings/marsExploration.cs
@@ -2,6 +2,7 @@
 
 public class marsExploration
 {
+    //solution 1
     public static int marsExploration(string s)
     {
         int result = 0;
@@ -18,6 +19,18 @@ public class marsExploration
                 if (s[i] != 'S')
                     result++;
             }
+        }
+        return result;
+    }
+
+    //solution 2
+    public static int marsExploration2(string s)
+    {
+        int result = 0;
+        string word = "SOS";
+        for (int i = 0; i < s.Length; i++){
+            if (s[i] != word[i % 3])
+                result++;
         }
         return result;
     }
